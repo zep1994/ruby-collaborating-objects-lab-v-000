4a0a9c65caa0cac2a0f126ba245c0a8ef491ae1f
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 12:09:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,10 +22,10 @@ class Artist
   def self.find(name)
     self.all.detect { |artist| artist.name == name }
   end
-+
-+  def self.find_or_create_by_name(name)
-+    self.find(name) || self.create(name)
-+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
    def add_song(song)
     @songs << song
    end
